35b90b907578bb663d38f7e00fe15b5e070bcc87
12300509 <alant_97@hotmail.com>
2014-11-12 18:36:51 -0600

    Update usuarios.php
    tabla de muestra bonita

--- usuarios.php
+++ usuarios.php
@@ -2,10 +2,45 @@
 <head>
 	<meta charset="UTF-8">
     <meta name="vieport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="animate.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    
+
+
+
+
+
 </head>
+
+
 <style type="text/css">
+.top
+{
+
+background-color:white ; 
+color: black;
+border-color: white;
+font-size: 30px;
+font-style: normal;
+
+}
+.tabla
+{
+	font-size: 20px;
+
+background-color: green;
+background-size: 3px;
+color: white;
+font-style:  oblique;
+
+}
+.border
+{
+border-width: medium;
+border-color: black;
+border-style: solid;
+
+
+}
 .jumbotron
 {
 	background-color: beige;
@@ -22,25 +57,49 @@ h1
 </style>
 <body class='background'>
 	<?php
-	$link = mysqli_connect('localhost', 'root', '','cars') or die('No se pudo conectar: ' . mysqli_error($link));
+	$link = mysqli_connect('localhost', 'root', '','cars') or die('No se pudo conectar: ' . mysql_error($link));
 
 	$query = 'SELECT * FROM persona';
-	$result = mysqli_query($link,$query) or die('Consulta fallida: ' . mysql_error());
+	$result = mysqli_query($link,$query) or die('Consulta fallida: ' . mysqli_error($link));
 	?>
 	<div class='jumbotron'><h1>Consulta Usuarios;<h1></div>
 	<div>
 		<?php
-			echo "<table>\n";
+		
+			echo '<table  align="center">';
+			echo '<tr class="top"><td class="border">id</td>';
+			echo '<td class="border">names</td>';
+			echo '<td class="border">midle_name</td>';
+			echo '<td class="border">last_name</td>';
+			echo '<td class="border">email</td></tr>';
 				while ($line = mysqli_fetch_array($result, MYSQL_ASSOC))
 				{
-    				echo "\t<tr>\n";
-    				foreach ($line as $col_value)
-    				{
-        				echo "\t\t<td>$col_value</td>\n";
-    				}
-    			echo "\t</tr>\n";
+    				echo '<tr class="tabla">';
+    				
+    				
+    					
+        				echo '<td class="border">';
+        				echo $line['id'];
+        				echo '</td>';
+        				echo '<td class="border">';
+        				echo $line['names'];
+        				echo '</td>';
+    					echo '<td class="border">';
+        				echo $line['midle_name'];
+        				echo '</td>';
+    					echo '<td class="border">';
+        				echo $line['last_name'];
+        				echo '</td>';
+    					echo '<td class="border">';
+        				echo $line['email'];
+        				echo '</td>';
+    				
+
+    				
+    			echo '</tr>';
 				}
-			echo "</table>\n";
+			echo '</table>';
+		
 		?>
 	</div>
 </body>
