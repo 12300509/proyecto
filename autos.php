7f92f8cb86997f43d60310addb5a7d917b2f1dc6
12300509 <alant_97@hotmail.com>
2014-11-12 18:54:39 -0600

    Update autos.php
    se agrego botón para regresar y diseño a tabla

--- autos.php
+++ autos.php
@@ -6,6 +6,34 @@
     
 </head>
 <style type="text/css">
+.top
+{
+
+background-color:white ; 
+color: black;
+border-color: white;
+font-size: 30px;
+font-style: normal;
+
+}
+.tabla
+{
+	font-size: 20px;
+
+background-color: green;
+background-size: 3px;
+color: white;
+font-style:  oblique;
+
+}
+.border
+{
+border-width: medium;
+border-color: black;
+border-style: solid;
+
+
+}
 .jumbotron
 {
 	background-color: beige;
@@ -23,29 +51,55 @@ h1
 <body class='background'>
 	<?php
 	$link = mysqli_connect('localhost', 'root', '','cars') or die('No se pudo conectar: ' . mysql_error($link));
-
 	$query = 'SELECT * FROM atributes';
 	$result = mysqli_query($link,$query) or die('Consulta fallida: ' . mysqli_error($link));
 	?>
 	<div class='jumbotron'><h1>Consulta Autos;<h1></div>
 	<div>
 		<?php
-			echo "<table>\n";
+			echo '<table align="center">';
+			echo '<tr class="top"><td class="border">id</td>';
+			echo '<td class="border">name</td>';
+			echo '<td class="border">color</td>';
+			echo '<td class="border">make</td>';
+			echo '<td class="border">model</td>';
+			echo '<td class="border">year</td></tr>';
 				while ($line = mysqli_fetch_array($result, MYSQL_ASSOC))
 				{
-    				echo "<tr>";
+    				echo '<tr class="tabla">';
     				
     				
     					
-        				echo "<td>";
+        				echo '<td class="border">';
         				echo $line['id'];
-        				echo "</td>";
+        				echo '</td>';
+        				echo '<td class="border">';
+        				echo $line['name'];
+        				echo '</td>';
+    					echo '<td class="border">';
+        				echo $line['color'];
+        				echo '</td>';
+    					echo '<td class="border">';
+        				echo $line['make'];
+        				echo '</td>';
+    					echo '<td class="border">';
+        				echo $line['model'];
+        				echo '</td>';
+        				echo '<td class="border">';
+        				echo $line['year'];
+        				echo '</td>';
     				
-    			echo "</tr>";
+    			
+
+
+
+
+    			echo '</tr>';
 				}
-			echo "</table>";
+			echo '</table>';
 			
 		?>
 	</div>
+		<div class='col-md-3'><input aling="center" type="button" value="REGRESAR" onClick="location.href = 'index.php'"><br></div>
 </body>
 </html>
