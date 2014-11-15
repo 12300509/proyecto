dcfa38b938eaa03c293812bc332c2984cbb33262
12300509 <alant_97@hotmail.com>
2014-11-14 18:10:35 -0600

    se agrego js

--- js/registrar_usuarios.js
+++ js/registrar_usuarios.js
@@ -1,36 +1,14 @@
 $(document).on('ready', function()
 {
+	//alert('whatever you want');
+		
+		var name = $('#name');
+	name.val("AWEBO AWEBO AWEBO");
+	name.change(function()
+	{
+		var midle_name = $("#midle_name");
+		midle_name.val($(this).val());
 
-	
-
-//alert('pop out');
-
-var name=$('#name');
-name.val('lo que sea');
-
-name.change(function()
-{
-
-	/*var midle_name=$('#midle_name');
-	console.log('algo');
-	midle_name.val($(this).val());
-*/
-
-if ($(this).val()=='65') {
-
-
-
-}
-else
- {
-alert('estas mal');
-
-}
-
-
-});
-
-
-
+	});
 
 });
