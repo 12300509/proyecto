4cce16f0d22e27b169618d023e12ed1273a456f0
12300509 <alant_97@hotmail.com>
2014-11-14 19:15:49 -0600

    se agrego validacion js a registrar usuarios

--- js/registrar_usuarios.js
+++ js/registrar_usuarios.js
@@ -1,14 +1,69 @@
 $(document).on('ready', function()
 {
-	//alert('whatever you want');
-		
+	
 		var name = $('#name');
-	name.val("AWEBO AWEBO AWEBO");
+		var midle_name = $('#midle_name');
+		var last_name = $('#last_name');
+		var numeros="0123456789";
+		var mail = $('#mail');
+
+
+		function tiene_numeros(texto)
+		{
+			 for(i=0; i<texto.length; i++){
+      if (numeros.indexOf(texto.charAt(i),0)!=-1){
+      	alert('sin numeros');
+      }
+   }
+         
+   alert("nada");
+}
+	
 	name.change(function()
 	{
-		var midle_name = $("#midle_name");
-		midle_name.val($(this).val());
+	tiene_numeros("$(this).value()");
+
+		if($(this).val().length < 4)
+		{
+
+			alert('nombre muy corto');
+			$(this).val("");
+		}
 
 	});
 
+		mail.change(function()
+		{
+
+		if($("#mail").val().indexOf('@', 0) == -1 || $("#mail").val().indexOf('.', 0) == -1) {  
+		            alert("mail incorrecto"); 
+		            $(this).val(""); 
+		            return false;  
+		        } 
+
+		});
+
+			midle_name.change(function()
+				{
+					if($(this).val().length < 4)
+					{
+
+						alert("apellido muy corto");
+						$(this).val("");
+					}
+
+
+				});
+
+				last_name.change(function()
+				{
+					if($(this).val().length < 4)
+					{
+
+						alert("apellido muy corto");
+						$(this).val("");
+					}
+
+
+				});
 });
--- registrar_usuarios.php
+++ registrar_usuarios.php
@@ -7,6 +7,22 @@
     <script src='js/registrar_usuarios.js'></script>
 </head>
 <style type="text/css">
+.errores{
+    -webkit-boxshadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    -o-box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    border-radius:10px;
+    -webkit-border-radius: 10px;
+    background: red;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    color: #fff;
+    display: none;
+    font-size: 12px;
+    margin-top: -40px;
+    margin-left: 250px;
+    padding: 10px;
+    position: absolute;
+}
 .jumbotron
 {
     background-color: beige;
@@ -31,6 +47,7 @@ h1
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese el nombre(s)</div>
                     <div class='col-md-3'><input id="name" type="text" name="name" value=""/><br></div>
+                    <div id="mensaje1" class="errores"> Ingresa solo caracteres</div>
                 </div>
             </div>
             <div class='row'>
@@ -38,6 +55,7 @@ h1
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese el apellido paterno</div>
                     <div class='col-md-3'><input id='last_name' type="text" name="last_name" value=""/><br></div>
+                     <div id="mensaje1" class="errores"> Ingresa solo caracteres</div>
                 </div>
             </div>
             <br>
@@ -46,6 +64,7 @@ h1
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese el apellido materno</div>
                     <div class='col-md-3'><input id="midle_name" type="text" name="midle_name" value=""/><br></div>
+                     <div id="mensaje1" class="errores"> Ingresa solo caracteres</div>
                 </div>
             </div>
             <br>
@@ -54,13 +73,14 @@ h1
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese e-mail</div>
                     <div class='col-md-3'><input id="mail" type="text" name="mail" value=""/><br></div>
+                    <div id="mensaje3" class="errores"> Mail no valido</div>
                 </div>
             </div>
             <br>
             <div class='row'>
                 <div class='col-md-3'></div>
                 <div class='col-md-9'>
-                    <div class='col-md-3'><input type="submit" value="Send" /><br></div>
+                    <div class='col-md-3'><input id="boton"type="submit" value="Send" /><br></div>
                 </div>
             </div>
         </form>
