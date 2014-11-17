5a077d2c507fb2ce37c0803688be793f7c5804da
edgarsandoval <edgarsandovalavalos@hotmail.com>
2014-11-17 13:51:34 -0600

    Update registrar_sucursales.php

--- registrar_sucursales.php
+++ registrar_sucursales.php
@@ -2,7 +2,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="vieport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <script type="text/javascript" src="js/jquery-2.1.1.js"></script>
+    <script type="text/javascript" src="js/registrar_sucursales.js"></script>
    
 </head>
 <style type="text/css">
@@ -29,14 +31,14 @@ h1
                 <div class='col-md-3'></div>
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese el nombre</div>
-                    <div class='col-md-3'><input type="text" name="name" value=""/><br></div>
+                    <div class='col-md-3'><input id="name" type="text" name="name" value=""/><br></div>
                 </div>
             </div>
             <div class='row'>
                 <div class='col-md-3'></div>
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese la direccion</div>
-                    <div class='col-md-3'><input type="text" name="address" value=""/><br></div>
+                    <div class='col-md-3'><input id="address" type="text" name="address" value=""/><br></div>
                 </div>
             </div>
             <br>
@@ -44,7 +46,7 @@ h1
                 <div class='col-md-3'></div>
                 <div class='col-md-9'>
                     <div class='col-md-3'>Ingrese el telefono</div>
-                    <div class='col-md-3'><input type="text" name="phone" value=""/><br></div>
+                    <div class='col-md-3'><input id="phone" type="text" name="phone" value=""/><br></div>
                 </div>
             </div>
             <br>
