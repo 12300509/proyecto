ddac435286564fb8a79b21cb757bffee42d9b1a8
edgarsandoval <edgarsandovalavalos@hotmail.com>
2014-11-17 13:42:07 -0600

    Update registrar_autos.js

--- js/registrar_autos.js
+++ js/registrar_autos.js
@@ -1,13 +1,88 @@
 $(document).on('ready', function()
 {
-	//alert('whatever you want');
+	
 		var name = $('#name');
-	name.val("AWEBO AWEBO AWEBO");
-	name.change(function()
-	{
-		var color = $("#color");
-		color.val($(this).val());
+		var color = $('#color');
+		var mark = $('#mark');
+		var model = $('#model');
+		var year = $('#year'); 
+		var numeros="0123456789";
+
+
+		function tiene_numeros(texto)
+		{
+			var a=0;
+			 for(i=0; i<texto.length; i++)
+			 {
+      				if (numeros.indexOf(texto.charAt(i),0)!=-1)
+      				a++;
+      				if(a>0)
+      					i=texto.length;
+			 }
+			 reuturn a;
+		}
+		name.change(function()
+		{
+			if(tiene_numeros("$(this).value()"))
+				alert("Este espacio no admite numeros");
+		if($(this).val().length < 1)
+		{
+
+			alert('El espacio está vacío');
+			$(this).val("");
+		}
 
 	});
 
+			color.change(function()
+				{
+					if(tiene_numeros("$(this).value()"))
+						alert("Este espacio no admite numeros");
+					if($(this).val().length < 1)
+					{
+
+						alert('El espacio está vacío');
+						$(this).val("");
+					}
+
+
+				});
+
+				mark.change(function()
+				{
+					if(tiene_numeros("$(this).value()"))
+						alert("Este espacio no admite numeros");
+					if($(this).val().length < 1)
+					{
+
+						alert('El espacio está vacío');
+						$(this).val("");
+					}
+
+
+				});
+				model.change(function()
+				{
+					if(tiene_numeros("$(this).value()"))
+						alert("Este espacio no admite numeros");
+					if($(this).val().length < 1)
+					{
+
+						alert('El espacio está vacío');
+						$(this).val("");
+					}
+
+
+				});
+				year.change(function()
+				{
+					if(!(tiene_numeros("$(this).value()")))
+						alert("Este espacio no admite caracteres");
+					if($(this).val().length < 1)
+					{
+
+						alert('El espacio está vacío');
+						$(this).val("");
+					}
+				});
 });
